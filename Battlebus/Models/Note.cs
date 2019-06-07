0d9dfeaef700a6f51648f9edbf0e651ba11e2506
unknown <bramosjaneway@gmail.com>
2019-06-07 11:42:21 -0500

    set up database

--- Battlebus/Models/Note.cs
+++ Battlebus/Models/Note.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Battlebus.Models
 {
-    public class Notes
+    public class Note
     {
+        [Key]
+        public int NoteId { get; set; }
+
         public string Content { get; set; }
 
         public SavedPolitician SavedPolitician { get; set; }
--- Battlebus/Models/SavedPolitician.cs
+++ Battlebus/Models/SavedPolitician.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,9 @@ namespace Battlebus.Models
 {
     public class SavedPolitician
     {
+        [Key]
+        public int SavedPoliticianId { get; set; }
+
         public string Name { get; set; }
 
         public string Photo { get; set; }
